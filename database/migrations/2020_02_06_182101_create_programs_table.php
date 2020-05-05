eb30fc6b9d3aec9e8e2257506c723b151f605997
Bosiipo <olabosipo07@gmail.com>
2020-05-05 08:55:16 +0100

    updated migrations

--- database/migrations/2020_02_06_182101_create_programs_table.php
+++ database/migrations/2020_02_06_182101_create_programs_table.php
@@ -17,7 +17,6 @@ class CreateProgramsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('duration');
-            $table->text('description');
             $table->timestamps();
         });
     }
